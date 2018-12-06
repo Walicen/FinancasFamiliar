4ab3504d083176ecead51706bbed4b9eddb9ffaa
diegodenzer <diegodenzer@hotmail.com>
2018-12-06 11:41:40 -0200

    uma pronto

--- contas/management/commands/alertapagamento.py
+++ contas/management/commands/alertapagamento.py
@@ -13,15 +13,31 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for op in options['operacao']:
             if op == 1:
-                faturas = list(Fatura.objects.filter(data_vencimento__lt=date.today(), status__isnull=True, tipo_fatura='D'))
-                for fatura in faturas:
 
-                    send_mail(
-                        'Teste 1',  # Assunto
-                        'Atrasada {}'.format(fatura.descricao),  # Corpo
-                        'diegodenzer.devops@gmail.com',
-                        ['diegodenzer.devops@gmail.com']
-                    )
-                    self.stdout.write(self.style.SUCCESS('Enviando'))
+                atrasadas = list(Fatura.objects.filter(data_vencimento__lt=date.today(), status__isnull=True, tipo_fatura='D'))
+                vencimentos_hoje = list(Fatura.objects.filter(data_vencimento=date.today(), status__isnull=True, tipo_fatura='D'))
+                lista = []
+                lista.append('Atrasadas: ')
+                if atrasadas:
+                    for fatura in atrasadas:
+                        lista.append('\n {} - valor: {} R$'.format(fatura.descricao, fatura.valor_fatura))
+                else:
+                    lista.append('\n Sem Lançamentos atrasados\n')
+
+                lista.append('Vencimentos Hoje: ')
+
+                if vencimentos_hoje:
+                    for fatura in vencimentos_hoje:
+                        lista.append('\n {} - valor: R$'.format(fatura.descricao, fatura.valor_fatura))
+                else:
+                    lista.append('\n Sem Lançamentos atrasados\n')
+
+                send_mail(
+                    'Financeiro Familiar',  # Assunto
+                    ''.join(lista),  # Corpo
+                    'diegodenzer.devops@gmail.com',
+                    ['diegodenzer.devops@gmail.com']
+                )
+                self.stdout.write(self.style.SUCCESS('Enviando'))
 
 
